b32ed80c67cc89aab12f11393495cc9998a645b0
kauefraga <kauefragarodrigues456@gmail.com>
2022-04-08 23:50:16 -0300

    feat(trolls): work with dirs

---
+++ trolls/RecursionOfDirs.bat
@@ -0,0 +1,17 @@
+:: It's so fine!
+:: BE AWARE OF YOUR ACTIONS!
+
+@echo off
+color 2
+cls
+
+set /A n=0
+
+echo Loop will start now
+:init
+mkdir folder%n%
+cd folder%n%
+
+set /A n+=1
+goto init
+
