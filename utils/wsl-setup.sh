267c3b39842178825905e431a9b55e286fe5c577
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-11-23 15:46:53 -0300

    :sparkles: feat: add rustup zsh ohmyzsh

--- utils/wsl-setup.sh
+++ utils/wsl-setup.sh
@@ -6,10 +6,20 @@
 #  | $$  $$  | $$__/
 #  | $$\  $$ | $$
 #  | $$ \  $$| $$      Author: Kauê Fraga Rodrigues
-#  |__/  \__/|__/      github.com/kauefraga/my-dotfiles
+#  |__/  \__/|__/      github.com/kauefraga/edc
 
-green='\033[0;32m'
-nocolor='\033[0m'
+# => ubuntu
+
+# colors
+nocolor='\033[0m'         # Color reset
+black='\033[0;30m'        # Black
+red='\033[0;31m'          # Red
+green='\033[0;32m'        # Green
+yellow='\033[0;33m'       # Yellow
+blue='\033[0;34m'         # Blue
+purple='\033[0;35m'       # Purple
+cyan='\033[0;36m'         # Cyan
+white='\033[0;37m'        # White
 
 Print() {
   echo -e "${green}[Ana] $@ ${nocolor}"
@@ -17,9 +27,18 @@ Print() {
 
 clear
 
-echo '*****************************************'
-echo '*    Relax! Ana will take control...    *'
-echo '*****************************************'
+echo -e $purple'******************************************'
+echo -e $purple'*                                        *'
+echo -e $purple'*    /$$   /$$ /$$$$$$$$                 *'
+echo -e $purple'*   | $$  /$$/| $$_____/                 *'
+echo -e $purple'*   | $$ /$$/ | $$                       *'
+echo -e $purple'*   | $$$$$/  | $$$$$                    *'
+echo -e $purple'*   | $$  $$  | $$__/                    *'
+echo -e $purple'*   | $$\  $$ | $$                       *'
+echo -e $purple'*   | $$ \  $$| $$  gh:kauefraga/edc     *'
+echo -e $purple'*   |__/  \__/|__/  Kauê Fraga Rodrigues *'
+echo -e $purple'*                                        *'
+echo -e $purple'******************************************'
 
 # Install fnm (node version manager)
 curl -fsSL https://fnm.vercel.app/install | bash
@@ -30,5 +49,21 @@ fnm install --lts
 fnm use lts-latest
 Print 'done'
 
-
+Print 'installing rustup...'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+Print 'done'
+
+Print 'installing zsh and oh my zsh...'
+apt install zsh -y > /dev/null
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+Print 'done'
+
+Print 'configuring zsh...'
+chsh --shell "$(which zsh)"
+Print $yellow'=> plugins:'
+Print $yellow'open your ~/.zshrc'
+Print $yellow'plugins=(git fnm python docker docker-compose rust zsh-syntax-highlighting zsh-autosuggestions)'
+
+Print $yellow'=> themes:'
+Print $yellow'avit, awesomepanda, garyblessington, gozilla, oxide, pi, elessar'
+Print 'done'
