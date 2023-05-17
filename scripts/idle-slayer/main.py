c276f138b96be208e92983aa6a6a38198997f018
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-05-17 20:33:21 -0300

    :recycle: refactor: change the default delay, use mousedown/mouseup instead of click and comment stuff

--- scripts/idle-slayer/main.py
+++ scripts/idle-slayer/main.py
@@ -1,25 +1,42 @@
+"""A script made with pyautogui to play Idle Slayer automatically."""
 import pyautogui
 import keyboard
 from numpy.random import uniform
 
 # Default delay
-pyautogui.PAUSE = 1
+pyautogui.PAUSE = 0.1
 
 # Alert people that the program is running
 print('The program is running...')
 
 
 def click(x: int, y: int):
-  pyautogui.click(x, y, button='left')
+  pyautogui.mouseDown(
+    x, y,
+    button='left',
+    duration=0.75
+  )
   pyautogui.sleep(uniform(0.1, 0.3))
+  pyautogui.mouseUp()
 
 while keyboard.is_pressed('q') != True:
-  if pyautogui.pixel(100, 630)[2] != 50:
-    click(100, 630)
-    # x 100 y 630 rgb(7, 25, 50)
+  # Sprint
+  if pyautogui.pixel(100, 630)[2] != 52:
+    # print(pyautogui.pixel(100,  630))
 
+    pyautogui.click(100, 630, button='left')
+
+  # Jump and shoot with bow
   click(650, 380)
 
 
 # Alert that the program finished
 print('The program has finished')
+
+"""
+1. if the portal is available, use it (x1240, y90 rgb(40, 1, 46))
+2. if some key is pressed, open the ascension menu and then:
+  2.1. check the "vassalos" menu
+  2.2. ascend
+3. log the main information
+"""
