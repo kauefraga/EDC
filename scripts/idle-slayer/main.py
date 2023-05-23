2ba55aaa3af0cc32d88c8f795fdd9094f1e7962c
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-05-22 23:24:55 -0300

    :recycle: refactor: encapsulate the main function, define settings and output the initial and the end time

--- scripts/idle-slayer/main.py
+++ scripts/idle-slayer/main.py
@@ -2,14 +2,8 @@
 # screen 1366x768
 import pyautogui
 import keyboard
+from datetime import datetime
 from numpy.random import uniform
-
-# Default delay
-pyautogui.PAUSE = 0.1
-
-# Alert people that the program is running
-print('The program is running...')
-
 
 def click(x: int, y: int):
   pyautogui.mouseDown(
@@ -20,31 +14,40 @@ def click(x: int, y: int):
   pyautogui.sleep(uniform(0.1, 0.3))
   pyautogui.mouseUp()
 
-while keyboard.is_pressed('q') != True:
-  # Get offline rewards
-  if pyautogui.pixel(710, 15)[2] == 0:
-    pyautogui.click(710, 15, button='left')
-    pyautogui.sleep(uniform(0.1, 0.3))
+def main():
+  # Alert people that the program is running
+  print(f'[script] The program is running... - {datetime.now().time()}')
 
-  # Check if the portal is available
-  if pyautogui.pixel(1240, 90)[2] == 153:
-    # click in the portal icon
-    pyautogui.click(1240, 90)
-    pyautogui.sleep(uniform(0.1, 0.3))
-    # press "yes"
-    pyautogui.click(580, 590)
-    pyautogui.sleep(uniform(0.5, 1))
+  while keyboard.is_pressed('q') != True:
+    # Get offline rewards
+    if pyautogui.pixel(710, 15)[2] == 0:
+      pyautogui.click(710, 15, button='left')
+      pyautogui.sleep(uniform(0.1, 0.3))
 
-  # Sprint
-  if pyautogui.pixel(100, 630)[2] == 155:
-    pyautogui.click(100, 630, button='left')
+    # Check if the portal is available
+    if pyautogui.pixel(1240, 90)[2] == 153:
+      # click in the portal icon
+      pyautogui.click(1240, 90)
+      pyautogui.sleep(uniform(0.1, 0.3))
+      # press "yes"
+      pyautogui.click(580, 590)
+      pyautogui.sleep(uniform(0.5, 1))
 
-  # Jump and shoot with bow
-  click(650, 380)
+    # Sprint
+    if pyautogui.pixel(100, 630)[2] == 155:
+      pyautogui.click(100, 630, button='left')
 
+    # Jump and shoot with bow
+    click(650, 380)
 
-# Alert that the program finished
-print('The program has finished')
+  # Alert that the program finished
+  print(f'[script] The program has finished - {datetime.now().time()}')
+
+if __name__ == '__main__':
+  # Settings
+  pyautogui.PAUSE = 0.1 # delay after all the commands
+
+  main()
 
 """
 1. if some key is pressed, open the ascension menu and then:
